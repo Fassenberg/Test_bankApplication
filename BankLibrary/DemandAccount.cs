f9c6b969031d1b79caa91b28bacfd48f006d6060
Artyom Rezvetsov <rezvetz@gmail.com>
2019-07-02 16:52:37 +0300

    Добавлены классы: * DemandAccount - счет по требованию * DepositAccount - депозитный счет Оба класса наследуют функционал абстрктного Account

---
+++ BankLibrary/DemandAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый счёт до востребования! Id счёта: {_id}", _sum));
+        }
+    }
+}
