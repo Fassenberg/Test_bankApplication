2194db518895deaf43e0082bdc9a17334520df5b
Artyom Rezvetsov <rezvetz@gmail.com>
2019-07-02 12:59:10 +0300

    Определен интерфейс для с методами пополнения и вывода средств со счета

---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    interface IAccount
+    {
+        /// <summary>
+        /// Положить деньги на счёт
+        /// </summary>
+        /// <param name="sum"></param>
+        void Put(decimal sum);
+        /// <summary>
+        /// Списать деньги со счета
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        decimal Withdraw(decimal sum);
+    }
+}
